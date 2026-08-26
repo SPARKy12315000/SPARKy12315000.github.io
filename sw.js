d6bbba42c29e3215dcfb8762fd8e41e40b96cd63
SPARKy12315000 <SPARKTOKEN@TUTAMAIL.COM>
2026-08-27 02:31:23 +0800

    chore: 移除旧 sw.js（干扰缓存/路由）

--- sw.js
+++
@@ -1,5 +0,0 @@
-const CACHE='spark-dapp-v2';
-const ASSETS=['./','./index.html','./apk.html','./manifest.json'];
-self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()));});
-self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
-self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{const c=res.clone();caches.open(CACHE).then(cache=>cache.put(e.request,c));return res;}).catch(()=>caches.match('./index.html'))));});
